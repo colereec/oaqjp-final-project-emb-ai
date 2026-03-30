dfc1a158192b5a0b39b9acc8f159035798da9cbb
colereec <ReeceDavidCole@gmail.com>
2026-03-30 01:15:10 -0400

    Completed Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,7 @@
-# Import requests library to handle HTTP requests.
+#Import requests library to handle HTTP requests.
 import requests
+#Import json library to hand JSON parsing.
+import json
 
 #Function to detect emotion of input text.
 def emotion_detector(text_to_analyze):
@@ -15,5 +17,24 @@ def emotion_detector(text_to_analyze):
     #Send a POST request to the API with the text for analysis and headers.
     response = requests.post(url, json = input_text, headers = header)
 
-    #Return the response from the API.
-    return response.text
+    #Parse the JSON response from the API call.
+    formatted_response = json.loads(response.text)
+
+    #Extract emotion scores.
+    emotion_predictions = formatted_response['emotionPredictions'][0]
+    anger_score = emotion_predictions['emotion']['anger']
+    disgust_score = emotion_predictions['emotion']['disgust']
+    fear_score = emotion_predictions['emotion']['fear']
+    joy_score = emotion_predictions['emotion']['joy']
+    sadness_score = emotion_predictions['emotion']['sadness']
+
+    #Calculate dominant emotion.
+    dominant_emotion = max(emotion_predictions['emotion'], key=emotion_predictions['emotion'].get)
+
+    #Return a dictonary with the emotions, their scores, and the dominant emotion.
+    return {'anger' : anger_score,
+    'disgust' : disgust_score,
+    'fear' : fear_score,
+    'joy' : joy_score,
+    'sadness' : sadness_score,
+    'dominant_emotion' : dominant_emotion}
